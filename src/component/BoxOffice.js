7119f33c632f6bbec36af0ca07498cf3b774a444
양정민 <yangjeongmin@yangjeongmin-ui-MacBookPro.local>
2023-01-27 21:52:18 +0900

    fri

--- src/component/BoxOffice.js
+++ src/component/BoxOffice.js
@@ -1,23 +1,93 @@
 import React from 'react'
+import { useState, useEffect } from "react";
 import './MovieDetail'
 import './Intro'
 import logo from '../img/BoxOffice/logo.png';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
-// import "./BoxOffice.css";
-
-// import required modules
 import { EffectCoverflow, Pagination,Mousewheel, Autoplay } from "swiper";
 import { Navigation } from "swiper";
 
 const BoxOffice = () => {
+
+  const KEY = "dcb877c29782034e77c94aac785199a2"
+  const DATE = "20230121"
+  
+
+  const [loading,setLoading] = useState(true);
+  const [rank,setRank] = useState([])
+  const [movies,setMovies] = useState([])
+  const getMovies = async() => {
+    const json = await (
+       await fetch(
+      `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${KEY}&targetDt=${DATE}`
+    )
+    ).json();
+    // console.log(json.boxOfficeResult.dailyBoxOfficeList);
+    const jsonArray = json.boxOfficeResult.dailyBoxOfficeList;
+    
+
+    let moviesData = [];
+    jsonArray.forEach(async(movie,key)=>{
+      let moveName = movie.movieNm;
+      let json2 = await (
+              await fetch(
+            `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=562TKI1106M3X3248YGA&detail=Y&title=${moveName}`
+          )
+          ).json();
+          
+          let max = 0;
+          json2.Data[0].Result.map((obj)=>{
+            if(Number(obj.modDate) > max)
+              max = Number(obj.modDate);
+            }
+          )
+
+        //   let max = 0;
+        //   json2.Data[0].Result.map((obj)=>{
+        //     if(obj.Codes.Code.CodeNo != '') {
+        //       // 코드끼리비교
+        //       console.log(obj.CodeNo == movie.movieCd);
+        //     } else {
+        //       //영문명 포함되는지 비교
+        //     }
+        // })
+
+          let data = json2.Data[0].Result.filter(obj=>{
+            obj.title = obj.title.replaceAll('!HS','');
+            obj.title = obj.title.replaceAll('!HE','');
+            obj.rank = movie.rank;
+            return Number(obj.modDate) == max
+          })
+          moviesData.push(...data);
+         
+          if(key == jsonArray.length-1) {
+            setTimeout(()=>{
+              moviesData.sort((a,b)=>{
+                let nA = Number(a.rank), nB = Number(b.rank);
+                if(nA > nB) return 1;
+                if(nA < nB) return -1;
+                return 0;
+              })
+              setMovies(moviesData);
+            },500)
+          }
+        })
+
+    // setRank(json.boxOfficeResult.dailyBoxOfficeList);
+    
+    // setLoading(false);
+  }
+  useEffect(() => {
+   getMovies();
+  }, [])
+
+  console.log(movies)
+
   return (
     <div className='boback'>
         <a href='/'><img className='logo' src={logo} alt='로고이미지'/></a>
@@ -87,53 +157,52 @@ const BoxOffice = () => {
       >  
 
           <div className='second-swiper'>
-          <SwiperSlide>
-            <div className='item'>
-              <div className='item-img'>
-                <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86729/86729_1000.jpg" alt='1' />
+            {movies&&movies.map((movie,key) =>
+            <SwiperSlide key={key}>
+              <div className='item'>
+                <div className='item-img'>
+                  <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86157/86157_1000.jpg" alt='1' />
+                </div>
+                <div className='hover-swipertxt'>
+                  <p><a href='./MovieDetail'>상세보기</a></p>
+                  <p><a href='https://www.cgv.co.kr/'>예매하기</a></p>
+                </div>
               </div>
-              <div className='hover-swipertxt'>
-                <p><a href='./MovieDetail'>상세보기</a></p>
-                <p><a href='https://www.cgv.co.kr/'>예매하기</a></p>
+              <div key={movie.movieCd} className='second-swipertxt'>
+                  <h4>{movie.rank}위</h4>
+                  <p>{movie.title}</p>
               </div>
-            </div>
-
-            <div className='second-swipertxt'>
-                <h4>1위</h4>
-                <p>아바타</p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86072/86072_1000.jpg" alt='2'/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86750/86750_1000.jpg" alt='3'/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86720/86720_1000.jpg" alt='4'/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86157/86157_1000.jpg" alt='5'/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86751/86751_1000.jpg" alt='6'/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86728/86728_1000.jpg" alt='7'/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86754/86754_1000.jpg" alt='8'/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86745/86745_1000.jpg" alt='9'/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000083/83203/83203_1000.jpg" alt='10'/>
-          </SwiperSlide>
+            </SwiperSlide>)}
+            
+            {/* <SwiperSlide>
+              <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86072/86072_1000.jpg" alt='2'/>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86750/86750_1000.jpg" alt='3'/>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86720/86720_1000.jpg" alt='4'/>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86157/86157_1000.jpg" alt='5'/>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86751/86751_1000.jpg" alt='6'/>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86728/86728_1000.jpg" alt='7'/>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86754/86754_1000.jpg" alt='8'/>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86745/86745_1000.jpg" alt='9'/>
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className='boxofficesp' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000083/83203/83203_1000.jpg" alt='10'/>
+            </SwiperSlide> */}
           </div>
       </Swiper>
-
-      
     </div>
   )
 }
--- src/component/MovieDetail.js
+++ src/component/MovieDetail.js
@@ -6,12 +6,36 @@ const MovieDetail = () => {
   return (
     <div className='mdback'>
         <a href='/'><img className='logo' src={logo} alt='로고이미지'/></a>
+        <div className='background-img'>
+          <img src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86072/86072_1000.jpg" alt='배경이미지'/>
+        </div>
+        <div className='background-black'></div>
 
         <div className='detail-main'>
             <div className='movie-txt'>Movie</div>
-            <div>
-                <div>
-                    <img className='poster' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86072/86072_1000.jpg" alt='1' />
+            <div className='movie-content'>
+                <div className='movie-poster'>
+                  <img className='poster' src="https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86072/86072_1000.jpg" alt='1' />
+                </div>
+                <div className='content-txt'>
+                  <div className='text1'>
+                    <div className='title'>
+                      <p className='title-content1'>아바타</p>
+                      <p className='title-content2'>12</p>
+                    </div>
+                    <p className='title2'>Avatar : The Way of Water</p>
+                    <p className='title3'>장르 | 192분 | 2022.12.14</p>
+                  </div>
+                  <div className='text2'>
+                    <p>감독 : 제임스 카메론</p>
+                    <p>
+                      배우 : 조 샐다나, 샘 워싱턴, 시고니 위버, 우나 채플린,<br></br>
+                      지오바니 리비시, 스티븐 랭, 케이트 윈슬렛
+                    </p>
+                  </div>
+                  <div className='text3'>
+                    <p>예매하기</p>
+                  </div>
                 </div>
             </div>
         </div>
